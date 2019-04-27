68723d4b5dfb7ffadcffd89dd079e6658320c2c5
wilson.manzano <wilson.manzanosanz@gmail.com>
2019-04-27 15:51:24 -0500

    Added navbar, footer, and root

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Navbar from './components/ui/navbar/navbar';
+import Footer from './components/ui/footer/footer';
+import Root from './pages/root';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+class App extends Component {
+    render() {
+      return (
+        <div>
+          <Navbar/>
+          <Root/>
+          <Footer/>
+        </div>
+      );
+    }
+  }
+  
 export default App;
---
+++ src/pages/root.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Root extends Component{
+    render(){
+        return (
+            <div>This is the page replacer.</div>
+        );
+    }
+}
+
+export default Root;
