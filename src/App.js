a66d2775f6ac32a8e061da9cecb2bccf1e43ea50
Wilson Manzano <wilson.manzano@qrvey.com>
2019-05-17 17:55:47 -0500

    Added style to navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/ui/navbar/navbar';
 import Footer from './components/ui/footer/footer';
 import Root from './pages/root';
 
+import './assets/scss/main.scss';
+
 class App extends Component {
     render() {
       return (
--- src/components/ui/navbar/navbar.js
+++ src/components/ui/navbar/navbar.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 
+import logo from '../../../assets/img/logo.svg';
+import hambuger from '../../../assets/img/hambuger.svg';
+
+import './navbar.scss';
+
 class Navbar extends Component {
     render() {
       return (
-        <div>This is the Navbar.</div>
+        <nav className="navbar wrapper">
+          <img className="nav-hambunger" alt="toggle" id="navbar-hambuger" src={hambuger}/>
+          <span className="spacer"></span>
+          <img className="nav-logo" alt="logo" id="navbar-logo" src={logo}/>
+        </nav>
       );
     }
   }
